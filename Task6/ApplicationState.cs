164d2df4b1b2a44a6c2e066328f6bf0ca2114ba7
Andriy_Antonik <zadrotdd1995@gmail.com>
2021-10-06 08:59:47 +0300

    ApplicationState

---
+++ Task6/ApplicationState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class ApplicationState
+    {
+        public int LoginId { get; set; }
+        public int MaxSize { get; set; }
+
+        // Singleton part:
+
+        private static ApplicationState instance;
+
+        private ApplicationState()
+        { }
+
+        public static ApplicationState getInstance()
+        {
+            if (instance == null)
+                instance = new ApplicationState();
+            return instance;
+        }
+    }
+}
